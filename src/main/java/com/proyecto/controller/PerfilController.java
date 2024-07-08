7f0782485174a28cf067906b107244178bb2e90a
Bee28Gabz <gabrielgranera28@hotmail.com>
2024-07-08 01:50:28 -0600

    Integracion de perfil, sobre nosotros y servicios

--- src/main/java/com/proyecto/controller/PerfilController.java
+++ src/main/java/com/proyecto/controller/PerfilController.java
@@ -1,13 +1,12 @@
 package com.proyecto.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NosotrosController {
-    @GetMapping("/nosotros")
-    public String nosotros(){
-        return "nosotros";
+public class PerfilController {
+    @GetMapping("/perfil")
+    public String perfil() {
+        return "perfil";
     }
 }
--- src/main/java/com/proyecto/controller/ServiciosController.java
+++ src/main/java/com/proyecto/controller/ServiciosController.java
@@ -1,13 +1,13 @@
 package com.proyecto.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NosotrosController {
-    @GetMapping("/nosotros")
-    public String nosotros(){
-        return "nosotros";
+public class ServiciosController {
+
+    @GetMapping("/servicios")
+    public String servicios() {
+        return "servicios";
     }
 }
